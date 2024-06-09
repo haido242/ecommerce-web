0267f9d54b1377412e059b06a0689f082da12ea4
Đào Hữu Thạch <88587051+thachdao31@users.noreply.github.com>
2024-06-09 20:05:10 +0700

    update upload image

--- src/app/admin/product/form-modal-product/index.ts
+++ src/app/admin/product/form-modal-product/index.ts
@@ -1,77 +1,129 @@
-import { Component, Input, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { BaseComponent } from "@app/abstract/BaseComponent";
 import { NzModalRef, NZ_MODAL_DATA } from "ng-zorro-antd/modal";
-import {
-    AbstractControl,
-    FormBuilder,
-    FormControl,
-    FormGroup,
-    NG_VALUE_ACCESSOR,
-    NonNullableFormBuilder,
-    ValidatorFn,
-    Validators
-  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzUploadFile } from "ng-zorro-antd/upload";
+import { ProductService } from "@app/services/product.service";
 
 @Component({
-    selector: 'form-modal-product',
-    templateUrl: './index.html',
-    styleUrls: ['./index.scss'],
+  selector: 'form-modal-product',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss'],
 })
+export class FormModalProduct extends BaseComponent {
+  isEditing: boolean = false;
+  productData: any;
 
-export class FormModalProduct extends BaseComponent{
-    isEditing: boolean = false;
-    productData: any;
+  readonly nzModalData = inject(NZ_MODAL_DATA);
+  private modalRef = inject(NzModalRef);
 
-    readonly nzModalData = inject(NZ_MODAL_DATA);
+  productForm: FormGroup;
+  fileList: any[] = [];
 
-    productForm: FormGroup;
-    fileList: any[] = [];
-    // fake list cvategory
-    listCategory = [
-        {_id: '1', name: 'Category 1'},
-        {_id: '2', name: 'Category 2'},
-        {_id: '3', name: 'Category 3'},
-        {_id: '4', name: 'Category 4'},
-    ]
-    constructor( private fb: FormBuilder) { 
-        super();
+  beforeUpload = (file: NzUploadFile): boolean => {
+    setTimeout(() => {
+      this.fileList = this.fileList.concat(file);
+    }, 0);
+    return false;
+  };
+
+  // fake list category
+  listCategory = [
+    { _id: '6645868e6e14f2dfb2ba43e1', name: 'Smart Phone' },
+    { _id: '664587d259daa916321d1290', name: 'Smart watch' },
+    { _id: '665ad622c31c1f7c75d8c9aa', name: 'Phone' },
+  ]
+
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+    this.isEditing = this.nzModalData.isEditing;
+    this.productData = this.nzModalData.productData;
+    if (this.isEditing && this.productData) {
+      this.productForm.patchValue(this.productData);
+    }
+  }
+
+  initForm() {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required],
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+      status: ['', Validators.required],
+      stockQuantity: ['', Validators.required],
+      isPhysicalProduct: [false],
+      weight: ['', Validators.required],
+      length: ['', Validators.required],
+      width: ['', Validators.required],
+      height: ['', Validators.required],
+      productImage: [''] // Thêm productImage vào form group
+    });
+  }
+
+  getListCategory() {}
+
+  handleChange($event: any) {
+    console.log("event", $event);
+    console.log("fileList", this.fileList);
+  }
+
+  handleCancel() {
+    this.modalRef.close(); // Đóng modal khi nhấn cancel
+  }
+
+  handleOk() {
+    // if (this.productForm.invalid) {
+    //   this.productForm.markAllAsTouched();
+    //   return;
+    // }
+    if(this.isEditing) {
+        this.updateProduct();
+    } else {
+        this.addNewProduct();
+    }
+  }
+
+  addNewProduct() {
+    const formData = new FormData();
+    const file = this.fileList[0];
+    if (file) {
+      formData.append('productImage', file as any);
     }
 
-    ngOnInit(): void {
-        this.initForm();
-        this.isEditing = this.nzModalData.isEditing;
-        this.productData = this.nzModalData.productData;
-        console.log("isEditing", this.isEditing);
-        console.log("productData", this.productData);
-        if(this.isEditing && this.productData) {
-            this.productForm.patchValue(this.productData);
+    Object.keys(this.productForm.controls).forEach(key => {
+      formData.append(key, this.productForm.get(key)?.value);
+    });
+
+    console.log("formData", formData);
+    this.productService.createProduct(formData).subscribe((res: any) => {
+      console.log(res);
+      this.modalRef.close(); // Đóng modal khi sản phẩm được tạo thành công
+    }, (err: any) => {
+      console.log(err);
+    });
+  }
+
+    updateProduct() {
+        const formData = new FormData();
+        const file = this.fileList[0];
+        if (file) {
+        formData.append('productImage', file as any);
         }
+    
+        Object.keys(this.productForm.controls).forEach(key => {
+        formData.append(key, this.productForm.get(key)?.value);
+        });
+    
+        console.log("formData", formData);
+        this.productService.updateProduct(this.productData._id, formData).subscribe((res: any) => {
+        console.log(res);
+        this.modalRef.close(); // Đóng modal khi sản phẩm được cập nhật thành công
+        }, (err: any) => {
+        console.log(err);
+        });
     }
-
-    initForm() {
-        this.productForm = this.fb.group({
-            name: ['', Validators.required],
-            price: ['', Validators.required],
-            description: [''],
-            image: [''],
-            category: ['', Validators.required],
-            status: [''],
-            stockQuantity: ['', Validators.required],
-            isPhysicalProduct: [false],
-            weight: ['', Validators.required],
-            length: ['', Validators.required],
-            width: ['', Validators.required],
-            height: ['', Validators.required],
-        })
-    }
-
-    getListCategory() {
-        
-    }
-
-    handleChange($event: any) {
-        
-    }
-
-    public returnDataForm: (dataFormJson) => void = () => { }
 }
--- src/app/admin/product/index.ts
+++ src/app/admin/product/index.ts
@@ -5,87 +5,88 @@ import { ModalService } from '@app/dialog/antModal';
 import { FormModalProduct } from './form-modal-product';
 
 @Component({
-    selector: 'product-management',
-    templateUrl: './index.html',
-    styleUrls: ['./index.scss']
+  selector: 'product-management',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
 })
+export class ProductManagement extends BaseComponent {
+  listProducts: any[] = [];
+  keyWordSearch: string = '';
 
-export class ProductManagement extends BaseComponent{
-    listProducts: any[] = [];
+  constructor(
+    private productService: ProductService,
+    private modalService: ModalService
+  ) {
+    super();
+  }
 
-    constructor(
-        private productService: ProductService,
-        private modalService: ModalService
-    ) {
-        super();
-    }
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.getAllProducts();
+  }
 
-    ngOnInit(): void {
-        super.ngOnInit();
-        this.getAllProducts();
-    }
+  getAllProducts() {
+    console.log('Get all products');
+    this.productService.getProducts().subscribe((res: any) => {
+      this.listProducts = res.data as any;
+    });
+  }
 
-    getAllProducts() {
-        this.productService.getProducts().subscribe((res: any) => {
-            this.listProducts = res.data as any;
-            console.log(this.listProducts);
-        });
-    }
+  onBtnAddProduct(event?: any) {
+    const modal = this.modalService.openModalWithSize(
+      FormModalProduct,
+      { isEditing: false, productData: {} },
+      'Add New Product',
+      800,
+      () => {
+        this.getAllProducts(); // Làm mới danh sách sản phẩm sau khi thêm mới
+      },
+      () => {
+        console.log('Cancel');
+      }
+    );
+  }
 
+  onBtnLowStock() {
+    this.productService.getProductsLowStock().subscribe((res: any) => {
+      this.listProducts = res.data as any;
+    });
+  }
 
+  deleteProduct(id: any) {
+    this.productService.deleteProduct(id).subscribe((res: any) => {
+      this.getAllProducts();
+    }, (err: any) => { console.log(err); });
+  }
 
-    onBtnAddProduct(event?: any) {
-        const modal = this.modalService.openModalWithSize(
-            FormModalProduct,
-            { isEditing: false, productData: {} }
-            , 'Add New Product', 
-            800,
-            () => {
-                // lấy dữ liệu từ formodalproduct
-            },
-            () => {
-                console.log('Cancel');
-            });
-    }
+  onBtnDeleteProduct(id: any) {
+    console.log(id);
+    this.modalService.openModalConfirm(
+      'Delete Product',
+      'Are you sure you want to delete this product?',
+      () => {
+        this.deleteProduct(id);
+      },
+      () => {
+        console.log('Cancel');
+      });
+  }
 
-    onBtnLowStock() {
-        this.productService.getProductsLowStock().subscribe((res: any) => {
-            this.listProducts = res.data as any;
-        });
-    }
+  onBtnEditProduct(product: any) {
+    this.modalService.openModalWithSize(
+      FormModalProduct,
+      { isEditing: true, productData: product },
+      'Edit Product',
+      800,
+      () => {
+        this.getAllProducts(); // Làm mới danh sách sản phẩm sau khi chỉnh sửa
+      },
+      () => {
+        console.log('Cancel');
+      });
+  }
 
-    deleteProduct(id: any) {
-        this.productService.deleteProduct(id).subscribe((res: any) => {
-            this.getAllProducts();
-        }, (err: any) => { console.log(err); });
-    }
-
-    onBtnDeleteProduct(id: any) {
-        console.log(id);
-        this.modalService.openModalConfirm(
-            'Delete Product', 
-            'Are you sure you want to delete this product?', 
-            () => {
-                this.deleteProduct(id);
-            },
-            () => {
-                console.log('Cancel');
-            });
-    }
-
-    onBtnEditProduct(product: any) {
-        this.modalService.openModalWithSize(
-            FormModalProduct,
-            { isEditing: true, productData: product }
-            , 'Edit Product', 
-            800,
-            () => {
-                console.log('OK');
-            },
-            () => {
-                console.log('Cancel');
-            });
-    }
-
-
+  onSearchProduct($event) {
+    
+  }
 }
